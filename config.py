7f84222d16df9b896fd1a928a02e12be4156714f
nikhil1w <rayrambadan70@gmail.com>
2025-08-03 06:09:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
       API_ID = int(getenv("API_ID", "24869695"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
       BOT_TOKEN = getenv("BOT_TOKEN", "8490857707:AAGrJ21XON-hAX161XANp0za4fsdKbV-ACU")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002702049353").replace("\n", " ").split(' '))
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002702049353:-/connect -1002733606326").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
